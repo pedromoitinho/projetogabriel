3fb479fecfe2f80bf4379ec0a761779ce6423180
pedromoitinho <pedrohenriquemoitinho.prado@ucsal.edu.br>
2024-11-06 22:23:27 -0300

    Despejo de arquivos no git

---
+++ script/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const audio = document.querySelector('audio');
+    audio.muted = false;
+    audio.play();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('a');
+    const loadingOverlay = document.getElementById('loadingOverlay');
+  
+    links.forEach(link => {
+      link.addEventListener('click', (e) => {
+        if (link.href) {
+          e.preventDefault();
+          loadingOverlay.style.display = 'flex';
+          
+          setTimeout(() => {
+            window.location.href = link.href;
+          }, 1000);
+        }
+      });
+    });
+  });
